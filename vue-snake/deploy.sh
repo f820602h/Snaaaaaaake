966c3849552c24031c73047f3f3a3f66d6353226
李炳寬 <f820602h@gmail.com>
2019-12-17 21:29:05 +0800

    fix deploy

--- vue-snake/deploy.sh
+++ vue-snake/deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:f820602h/Snaaaaaaake.git master:gh-pages
 
 cd -
